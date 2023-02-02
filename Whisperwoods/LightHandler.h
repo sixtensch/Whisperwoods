42c70c40721f5f4f7ca435e4158b1c1ae307b619
DANIEL HÄLL <dahl20@student.bth.se>
2023-02-02 14:34:54 +0100

    feat(Lights): Added light resources for the different light types

---
+++ Whisperwoods/LightHandler.h
@@ -0,0 +1,49 @@
+#pragma once
+enum LightType
+{
+	LIGHT_TYPE_DIRECTIONAL = 0,
+	LIGHT_TYPE_SPOTLIGHT = 1,
+	LIGHT_TYPE_POINTLIGHT = 2
+};
+constexpr int START_MAX_LIGHTS = 40;
+
+class PointLight
+{};
+class DirectionalLight
+{};
+class SpotLight
+{};
+
+class LightHandler sealed
+{
+public:
+	LightHandler() = delete;
+	LightHandler( ID3D11Device* device );
+	~LightHandler();
+
+
+	ID3D11Buffer* GetStructuredBufferP( LightType type );
+	ID3D11Buffer* const* GetStructuredBufferPP( LightType type );
+
+	
+	void SetDirectionalLights( ID3D11DeviceContext* immediateContext, std::vector<DirectionalLight> data );
+	void SetSpotLights( ID3D11DeviceContext* immediateContext, std::vector<SpotLight> data );
+	void SetPointLights( ID3D11DeviceContext* immediateContext, std::vector<PointLight> data );
+
+	bool ReallocLights( ID3D11Device* device, LightType type );
+
+private:
+	bool InitPointSBuffer( ID3D11Device* device, UINT LightCount );
+	bool InitDirSBuffer( ID3D11Device* device, UINT LightCount );
+	bool InitSpotSBuffer( ID3D11Device* device, UINT LightCount );
+
+
+public:
+private:
+	wrl::ComPtr<ID3D11Buffer> m_pointSBuffer;
+	wrl::ComPtr<ID3D11Buffer> m_spotSBuffer;
+	wrl::ComPtr<ID3D11Buffer> m_dirSBuffer;
+
+	int m_maxSize[3];
+};
+
