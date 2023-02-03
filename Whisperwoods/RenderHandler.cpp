f154bb25ad2281601fb2aad9b3066a40d7f87b86
DANIEL HÄLL <dahl20@student.bth.se>
2023-02-03 09:33:46 +0100

    chore(pipeline): fixed camera projection update

--- Whisperwoods/RenderHandler.cpp
+++ Whisperwoods/RenderHandler.cpp
@@ -13,6 +13,7 @@ void RenderHandler::InitCore(shared_ptr<Window> window)
 {
 	m_renderCore = make_unique<RenderCore>(window);
 	mainCamera.SetValues( 90 * dx::XM_PI/180, window->GetAspectRatio(),0.01f, 100.0f );
+	mainCamera.CalculatePerspectiveProjection();
 }
 
 void RenderHandler::Draw()
