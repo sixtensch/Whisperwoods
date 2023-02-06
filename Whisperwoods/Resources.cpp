0b1d09f55da0f0c882ca3e1670d4a32ea75d073b
Claiko <jonathan.dellova42@gmail.com>
2023-02-06 14:20:54 +0100

    feat(resource): Can now allocate specific resource and get specific resource.

--- Whisperwoods/Resources.cpp
+++ Whisperwoods/Resources.cpp
@@ -1,9 +1,13 @@
 #include "core.h"
 #include "Resources.h"
 
+#include "TextureResource.h"
+#include "ShaderResource.h"
+#include "SoundResource.h"
+
+
+
 Resources* Resources::s_singleton = nullptr;
-
-
 
 Resources::Resources()
 	: m_resourceMaps({})
@@ -17,6 +21,8 @@ Resources::Resources()
 
 	InitMapList();
 
+	// TODO: Dudd code. Remove later.
+	AllocateResource(TEXTURE, "TestPath/Test", "Test name");
 }
 
 Resources::~Resources()
@@ -43,3 +49,59 @@ void Resources::InitMapList()
 		m_resourceMaps.Add({});
 	}
 }
+
+const BasicResource* Resources::GetResource(const RESOURCE_TYPES resourceType, const std::string subPath) const
+{
+	auto& resourceMap = m_resourceMaps[resourceType];
+	auto it = resourceMap.find(subPath);
+
+	if (it != resourceMap.end())
+	{
+		return it->second.get();
+	}
+
+	return nullptr;
+}
+
+BasicResource* Resources::AllocateResource(const RESOURCE_TYPES resourceType, const std::string subPath, const std::string resourceName)
+{
+	std::shared_ptr<BasicResource> resource = nullptr;
+
+	switch (resourceType)
+	{
+	case SHADER:
+		resource = make_shared<ShaderResource>(resourceName);
+		break;
+
+	case TEXTURE:
+		resource = make_shared<TextureResource>(resourceName);
+		break;
+
+	case SOUND:
+		resource = make_shared<SoundResource>(resourceName);
+		break;
+
+	default:
+		return nullptr; // If not valid type, return nullptr.
+		break;
+	}
+
+	auto& resourceMap = m_resourceMaps[resourceType]; 
+	auto returnIt = resourceMap.insert({ subPath, std::move(resource) });
+
+	bool isSuccessful = returnIt.second;
+	const auto& insertionIt = returnIt.first;
+
+	if (isSuccessful == false)
+	{
+		std::string blockerName = insertionIt->second.get()->resourceName;
+		std::string blockerPath = insertionIt->first;
+		EXC("Failed to create resource of type '%s' with path '%s'. Prevented by resource '%s' with path '%s'", 
+			resourceName.c_str(), subPath.c_str(), blockerName.c_str(), blockerPath.c_str());
+	}
+	
+	return insertionIt->second.get();
+}
+
+
+
--- Whisperwoods/Resources.h
+++ Whisperwoods/Resources.h
@@ -4,6 +4,37 @@
 #include <unordered_map>
 
 #include "BasicResource.h"
+
+/*
+
+
+	Detta kan bli mer generaliserat med följande struktur:
+
+	cs::List<unordered_map<std::string, shared_ptr<BasicResource>>;
+
+	Där varje index definieras av någon resurstyp som vi har definerat i en enum:
+
+	enum RESOURCE_TYPE {
+		SHADER = 0,
+		TEXTURE, 
+		SOUND
+	}
+
+	Då kan generella funktioner skapas som:
+
+	const BasicResource* GetResource(ResourceType type, std::string subPath);  
+
+	.
+	.
+	.
+	
+
+
+	Ett till lager av säkerhet kan läggas på datan genom att returnera weak_ptrs. Dessa kan endast bli
+	castade till en shared_ptr OM den underliggande shared_ptr faktiskt existerar. Den äger på så sätt aldrig datan.
+
+*/
+
 enum RESOURCE_TYPES {
 	SHADER = 0,
 	TEXTURE,
@@ -13,6 +44,10 @@ enum RESOURCE_TYPES {
 
 typedef std::unordered_map<std::string, shared_ptr<BasicResource>> ResourceMap;
 
+// Define functions that COULD be defined if its too wordy to use casting etc etc.
+#define GET_TEXTURE(subPath) GetResource(TEXTURE, subPath);
+#define GET_SOUND(subPath) GetResource(SOUND, subPath);
+
 class Resources sealed
 {
 public:
@@ -21,10 +56,16 @@ public:
 
 	static Resources& Get();
 
+	const BasicResource* GetResource(const RESOURCE_TYPES resourceType, std::string subPath) const;
+
 private:
 
 	void InitMapList();
 	void InitializeTextures();
+
+	// Allocates a specific resource type in its specific map and returns a pointer to the allocated memory.
+	BasicResource* AllocateResource(const RESOURCE_TYPES resourceType, const std::string subPath, const std::string resourceName);
+
 private:
 	static Resources* s_singleton;
 
