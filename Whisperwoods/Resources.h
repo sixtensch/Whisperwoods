d41aaa4ef6342f5e4d0e64bdea2b4cd1a2122ad8
Claiko <jonathan.dellova42@gmail.com>
2023-02-06 14:26:38 +0100

    docs(resources): updated comment.

--- Whisperwoods/Resources.h
+++ Whisperwoods/Resources.h
@@ -6,30 +6,6 @@
 #include "BasicResource.h"
 
 /*
-
-
-	Detta kan bli mer generaliserat med följande struktur:
-
-	cs::List<unordered_map<std::string, shared_ptr<BasicResource>>;
-
-	Där varje index definieras av någon resurstyp som vi har definerat i en enum:
-
-	enum RESOURCE_TYPE {
-		SHADER = 0,
-		TEXTURE, 
-		SOUND
-	}
-
-	Då kan generella funktioner skapas som:
-
-	const BasicResource* GetResource(ResourceType type, std::string subPath);  
-
-	.
-	.
-	.
-	
-
-
 	Ett till lager av säkerhet kan läggas på datan genom att returnera weak_ptrs. Dessa kan endast bli
 	castade till en shared_ptr OM den underliggande shared_ptr faktiskt existerar. Den äger på så sätt aldrig datan.
 
