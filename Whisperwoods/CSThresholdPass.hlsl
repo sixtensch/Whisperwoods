9a8e0d34c6f0eea00a5cbb3c024bf5daea520418
DANIEL HÄLL <dahl20@student.bth.se>
2023-03-06 13:54:49 +0100

    feat(ComputeShader): Threshold pass regionaized

--- Whisperwoods/CSThresholdPass.hlsl
+++ Whisperwoods/CSThresholdPass.hlsl
@@ -35,26 +35,33 @@ cbuffer TIME_SWITCH_INFO_BUFFER : REGISTER_CBV_SWITCH_INFO
     bool PADDING[3];
 }
 
+#define REGION_SIZE_PER_THREAD 2
 [numthreads(NUM_THREADS.x, NUM_THREADS.y, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
     uint3 texPos = uint3(DTid.xy, 0);
+    texPos *= REGION_SIZE_PER_THREAD;
     
-    float3 texColor = renderTexture.Load(texPos).rgb;
-
-    float totalInflunce = 
-        TotalTimeSwitchInfluence(
-        timeSinceSwitch,
-        timeSwitchStartDuration,
-        timeSwitchEndDuration
-    );
-    
-    texColor = FilterBrightness(
-        texColor, 
-        lerp(luminanceThreshold, 0.0f, totalInflunce),
-        lerp(strength, strength * 1.4f, totalInflunce),
-        lerp(minLuminance, 0.0f, totalInflunce)
-    );
-    
-    lumTexture[texPos.xy] = float4(texColor, 1.0f);
+    [unroll]
+    for (uint y = texPos.y; y < texPos.y + REGION_SIZE_PER_THREAD; ++y)
+    [unroll]
+        for (uint x = texPos.x; x < texPos.x + REGION_SIZE_PER_THREAD; ++x)
+        {
+            float3 texColor = renderTexture.Load(texPos).rgb;
+            float totalInflunce = TotalTimeSwitchInfluence(
+                timeSinceSwitch,
+                timeSwitchStartDuration,
+                timeSwitchEndDuration
+            );
+            
+            texColor = FilterBrightness(
+                texColor,
+                lerp(luminanceThreshold, 0.0f, totalInflunce),
+                lerp(strength, strength * 1.4f, totalInflunce),
+                lerp(minLuminance, 0.0f, totalInflunce)
+            );
+            
+            uint2 coord = uint2(x, y);
+            lumTexture[coord] = float4(texColor, 1.0f);
+        }
 }
